177f09416b44d9a5450d4316268a4f1f2b63b37d
chenjunliang <chenjunliang@df-emall.com>
2022-08-09 21:16:13 +0800

    add author

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ mod tasks;
 fn main() {
 
     println!("pkg_name:{}", env!("CARGO_PKG_NAME"));
-    println!("author_name:{}", env!("CARGO_PKG_NAME").get(0).unwrap());
+    println!("author_name:{}", env!("CARGO_PKG_AUTHORS").get(0).unwrap());
 
     let username = libc_util::get_current_user();
     daemon_util::daemonize(tasks::start_backstage_task, username);
