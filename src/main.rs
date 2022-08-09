a91a14d213e741747da597c39fb8cf42abaebc5d
chenjunliang <chenjunliang@df-emall.com>
2022-08-09 20:24:19 +0800

    每秒执行测试

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
+use std::time::Duration;
+use rcron::{Job, JobScheduler};
 use crate::crontab_handle::init_crontab;
 
-mod crontab_handle;
 mod daemon_util;
 mod libc_util;
 mod tasks;
@@ -8,5 +9,20 @@ mod tasks;
 fn main() {
     // let username = libc_util::get_current_user();
     // daemon_util::daemonize(tasks::start_backstage_task, username);
-    init_crontab();
+
+    let mut sched = JobScheduler::new();
+
+    sched.add(Job::new("1/10 * * * * *".parse().unwrap(), || {
+        println!("exec task every 10 seconds!");
+    }));
+
+    sched.add(Job::new("1/5 * * * * *".parse().unwrap(), || {
+        println!("exec task every 5 seconds!");
+    }));
+
+    loop {
+        sched.tick();
+
+        std::thread::sleep(Duration::from_millis(500));
+    }
 }
