6705df65f79226dcbb321fc0616c882ea75dd3f7
chenjunliang <chenjunliang@df-emall.com>
2022-08-09 21:03:22 +0800

    每秒执行测试

--- src/tasks.rs
+++ src/tasks.rs
@@ -1,8 +1,9 @@
+use std::borrow::BorrowMut;
 use std::thread::sleep;
 use std::time::{Duration, SystemTime};
 use rcron::{Job, JobScheduler};
 
-fn mysql_audit_log_rotate(mut sched: JobScheduler) {
+fn mysql_audit_log_rotate(sched: &mut JobScheduler) {
     sched.add(Job::new("1/10 * * * * *".parse().unwrap(), || {
         println!("执行日志轮转任务!");
     }));
@@ -11,7 +12,7 @@ fn mysql_audit_log_rotate(mut sched: JobScheduler) {
 pub fn start_backstage_task() {
     let mut sched = JobScheduler::new();
 
-    mysql_audit_log_rotate(sched);
+    mysql_audit_log_rotate(sched.borrow_mut());
 
     loop {
         sched.tick();
