b99fa3e828bbe7943dddadbd56a7d66a4eeb7c74
chenjunliang <chenjunliang@df-emall.com>
2022-08-09 21:02:18 +0800

    每秒执行测试

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,11 @@ mod libc_util;
 mod tasks;
 
 fn main() {
+    let vars1 = std::env::vars();
+    for (k, v) in vars1 {
+        println!("env k:{}, v:{}", k, v);
+    }
+
     let username = libc_util::get_current_user();
     daemon_util::daemonize(tasks::start_backstage_task, username);
 }
--- src/tasks.rs
+++ src/tasks.rs
@@ -2,16 +2,16 @@ use std::thread::sleep;
 use std::time::{Duration, SystemTime};
 use rcron::{Job, JobScheduler};
 
+fn mysql_audit_log_rotate(mut sched: JobScheduler) {
+    sched.add(Job::new("1/10 * * * * *".parse().unwrap(), || {
+        println!("执行日志轮转任务!");
+    }));
+}
+
 pub fn start_backstage_task() {
     let mut sched = JobScheduler::new();
 
-    sched.add(Job::new("1/10 * * * * *".parse().unwrap(), || {
-        println!("exec task every 10 seconds!");
-    }));
-
-    sched.add(Job::new("1/5 * * * * *".parse().unwrap(), || {
-        println!("exec task every 5 seconds!");
-    }));
+    mysql_audit_log_rotate(sched);
 
     loop {
         sched.tick();
