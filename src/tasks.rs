db440ee2dc96ac31d8c443bb7f3fcc598621e399
chenjunliang <chenjunliang@df-emall.com>
2022-08-09 20:25:11 +0800

    每秒执行测试

--- src/tasks.rs
+++ src/tasks.rs
@@ -1,8 +1,7 @@
 use std::thread::sleep;
 use std::time::{Duration, SystemTime};
-use crate::crontab_handle::init_crontab;
 
 pub fn start_backstage_task() {
     println!("this is a test ... ");
-    init_crontab();
+
 }
