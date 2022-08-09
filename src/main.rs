51fece9201c144998a4ae44420085ce0dfa17810
chenjunliang <chenjunliang@df-emall.com>
2022-08-09 21:15:01 +0800

    add author

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,9 @@ mod libc_util;
 mod tasks;
 
 fn main() {
-    let vars1 = std::env::vars();
-    for (k, v) in vars1 {
-        println!("env k:{}, v:{}", k, v);
-    }
+
+    println!("pkg_name:{}", env!("CARGO_PKG_NAME"));
+    println!("author_name:{}", env!("CARGO_PKG_NAME").get(0).unwrap());
 
     let username = libc_util::get_current_user();
     daemon_util::daemonize(tasks::start_backstage_task, username);
