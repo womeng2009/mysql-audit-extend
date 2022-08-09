b17a675d4b16045d8d1bdb7ab46557522382b3fe
chenjunliang <chenjunliang@df-emall.com>
2022-08-09 20:36:00 +0800

    每秒执行测试

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,8 @@
-use std::time::Duration;
-use rcron::{Job, JobScheduler};
-
 mod daemon_util;
 mod libc_util;
 mod tasks;
 
 fn main() {
-    // let username = libc_util::get_current_user();
-    // daemon_util::daemonize(tasks::start_backstage_task, username);
-
-    let mut sched = JobScheduler::new();
-
-    sched.add(Job::new("1/10 * * * * *".parse().unwrap(), || {
-        println!("exec task every 10 seconds!");
-    }));
-
-    sched.add(Job::new("1/5 * * * * *".parse().unwrap(), || {
-        println!("exec task every 5 seconds!");
-    }));
-
-    loop {
-        sched.tick();
-
-        std::thread::sleep(Duration::from_secs(1));
-    }
+    let username = libc_util::get_current_user();
+    daemon_util::daemonize(tasks::start_backstage_task, username);
 }
--- src/tasks.rs
+++ src/tasks.rs
@@ -1,7 +1,21 @@
 use std::thread::sleep;
 use std::time::{Duration, SystemTime};
+use rcron::{Job, JobScheduler};
 
 pub fn start_backstage_task() {
-    println!("this is a test ... ");
+    let mut sched = JobScheduler::new();
 
+    sched.add(Job::new("1/10 * * * * *".parse().unwrap(), || {
+        println!("exec task every 10 seconds!");
+    }));
+
+    sched.add(Job::new("1/5 * * * * *".parse().unwrap(), || {
+        println!("exec task every 5 seconds!");
+    }));
+
+    loop {
+        sched.tick();
+
+        sleep(Duration::from_secs(1));
+    }
 }
