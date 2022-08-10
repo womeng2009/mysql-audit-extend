b6ec75f0c9749ff8d70baef371ca272fb539aa9d
chenjunliang <chenjunliang@df-emall.com>
2022-08-10 18:47:55 +0800

    原始名称截取

--- src/tasks.rs
+++ src/tasks.rs
@@ -32,7 +32,12 @@ fn mysql_audit_log_rotate(sched: &mut JobScheduler, path: String, max_size: u32,
 
                 let file_path = Path::new(path.as_str());
                 let origin_file_name = file_path.file_name().unwrap().to_str().unwrap().to_string();
-                println!("origin_file_name:{}", origin_file_name);
+                let origin_name = origin_file_name
+                    .split(".")
+                    .collect::<Vec<String>>()
+                    .get(0)
+                    .unwrap();
+                println!("origin_file_name:{}", origin_name);
 
                 let parent_path = file_path.parent().unwrap();
                 println!("parent_path:{}", parent_path.to_str().unwrap());
