33218c176fd3698f8d3389335e6f150a8d5f617a
chenjunliang <chenjunliang@df-emall.com>
2022-08-10 16:57:33 +0800

    文件限制参数

--- src/main.rs
+++ src/main.rs
@@ -15,11 +15,11 @@ struct Options {
     path: String,
 
     /// Maximum file size, Unit: MB.
-    #[clap(short = "ms", long, value_parser, default_value_t = 10)]
+    #[clap(short = 'm', long, value_parser, default_value_t = 10)]
     max_size: u32,
 
     /// Maximum number of files to keep
-    #[clap(short = "mf", long, value_parser, default_value_t = 10)]
+    #[clap(short = 'f', long, value_parser, default_value_t = 10)]
     max_file: u32,
 }
 
