e51a583eca2d59d044905b595e1ba88a7837412f
chenjunliang <chenjunliang@df-emall.com>
2022-08-10 16:20:41 +0800

    生命周期标注

--- src/tasks.rs
+++ src/tasks.rs
@@ -1,11 +1,11 @@
 use std::borrow::BorrowMut;
 use std::thread::sleep;
-use std::time::Duration;
+use std::time::{Duration, SystemTime};
 use rcron::{Job, JobScheduler};
 
 fn mysql_audit_log_rotate(sched: &mut JobScheduler, path: String) {
-    sched.add(Job::new("1/10 * * * * *".parse().unwrap(),  move || {
-        println!("执行日志轮转任务! path:{}", path);
+    sched.add(Job::new("0 24 8 * * *".parse().unwrap(),  move || {
+        println!("执行日志轮转任务! path:{}, time:{:?}", path, SystemTime::now());
     }));
 }
 
