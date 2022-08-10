c233d954a2255e6e6013a28e15676ff00c903699
chenjunliang <chenjunliang@df-emall.com>
2022-08-10 15:48:45 +0800

    开启任务

--- src/main.rs
+++ src/main.rs
@@ -29,7 +29,8 @@ fn main() -> Result<()> {
     let username = libc_util::get_current_user();
     let pkg_name = env!("CARGO_PKG_NAME");
     let author_name = env!("CARGO_PKG_AUTHORS");
-    daemon_util::daemonize(tasks::start_backstage_task, username.as_str(), pkg_name, author_name, options.path);
+    let path: &str = options.path.as_str();
+    daemon_util::daemonize(tasks::start_backstage_task, username.as_str(), pkg_name, author_name, path);
 
     Ok(())
 }
